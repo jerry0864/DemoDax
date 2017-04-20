729752279f526a1e808401db91075a1d37adfe91
liuxiong <liuxiong@corp.netease.com>
2017-04-20 20:43:15 +0800

    add test java clone

--- DemoJava/src/main/java/com/dax/java/Demo.java
+++ DemoJava/src/main/java/com/dax/java/Demo.java
@@ -1,14 +1,40 @@
 package com.dax.java;
 
+import com.dax.java.clone.Person;
+
 public class Demo {
     public static void main(String[] args){
-//        Haha haha = new Haha();
-//        Bala bala = new Bala();
-//        haha.a();
-//        haha.b();
-//        bala.a();
-//        bala.b();
+        //testClone();
+        //testStringCompare();
 
+    }
+
+    private static void testStringCompare() {
+        //java首先会在缓冲区查找是否有"aaa"这个常量对象，有就直接将其地址赋给a，没有就创建一个"aaa"，然后将其赋给b
+        String a = "aaa";
+        String b = "aaa";
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+
+        System.out.println("-------");
+
+        String c = new String("aaa");
+        String d = new String("aaa");
+        System.out.println(c==d);
+        System.out.println(c.equals(d));
+    }
+
+    private static void testClone() {
+        Person person = new Person();
+        person.name = "zhangsan";
+        person.age = 18;
+
+        Person person1 = person.clone();
+        person1.name = "zhangsan";
+        person.age = 18;
+        System.out.println(person == person1);
+        System.out.println(person.name == person1.name);
+        System.out.println(person.info == person1.info);
     }
 }
 
---
+++ DemoJava/src/main/java/com/dax/java/clone/Person.java
@@ -0,0 +1,29 @@
+package com.dax.java.clone;
+
+import java.util.HashMap;
+
+/**
+ * Desc:
+ * Created by liuxiong on 2017/4/20.
+ * Email:liuxiong@corp.netease.com
+ */
+
+public class Person implements Cloneable {
+    public String name;
+    public int age;
+    boolean flag;
+    public HashMap info = new HashMap();
+
+    @Override
+    public Person clone()  {
+        Person p = null;
+        try {
+            p = (Person) super.clone();
+            p.flag = true;
+            p.info = (HashMap) info.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
