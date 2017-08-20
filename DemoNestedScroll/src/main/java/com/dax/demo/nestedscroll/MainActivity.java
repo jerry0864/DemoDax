17e2476ac103d7a30a381e2fa6925b17f28a9e57
liuxiong <1774955251@qq.com>
2017-08-20 16:01:05 +0800

    add demonestedscroll

---
+++ DemoNestedScroll/src/main/java/com/dax/demo/nestedscroll/MainActivity.java
@@ -0,0 +1,92 @@
+package com.dax.demo.nestedscroll;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<String>data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        //show listview
+        data = new ArrayList();
+        for(int i = 0;i<60;i++){
+            data.add("test----> "+i);
+        }
+        ListView list = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,data.toArray());
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,String.valueOf(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+        //show SwipeRefreshLayout
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                },2000);
+            }
+        });
+
+        //show RecylerView
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//        recyclerView.setLayoutManager(layoutManager);
+//        RecyclerView.Adapter recyclerAdapter = new RecyclerView.Adapter(){
+//
+//            @Override
+//            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//                LinearLayout linearLayout = new LinearLayout(MainActivity.this);
+//                parent.addView(linearLayout);
+//                return new MyHolder(linearLayout);
+//            }
+//
+//            @Override
+//            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//                MyHolder holder1 = (MyHolder) holder;
+//                holder1.text.setText(data.get(position));
+//            }
+//
+//            @Override
+//            public int getItemCount() {
+//                return data.size();
+//            }
+//        };
+//        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+        private TextView text;
+        public MyHolder(View itemView) {
+            super(itemView);
+            LinearLayout linearLayout = (LinearLayout) itemView;
+            text = new TextView(MainActivity.this);
+            linearLayout.addView(text);
+        }
+    }
+}
---
+++ DemoSpinner/src/main/java/com/dax/demo/spinner/MainActivity.java
@@ -0,0 +1,75 @@
+package com.dax.demo.spinner;
+
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(new SpinnerAdapter() {
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
+    }
+}
