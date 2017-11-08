4c80dfc8bb38cff9b6311061c3c36874e53df15b
liuxiong <1774955251@qq.com>
2017-11-08 23:46:38 +0800

    add subsampleimageview

---
+++ DemoLongScreenshot/src/main/java/com/dax/demo/longscreenshot/LongScreenActivity.java
@@ -0,0 +1,27 @@
+package com.dax.demo.longscreenshot;
+
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+public class LongScreenActivity extends AppCompatActivity {
+    //https://github.com/Piasy/BigImageViewer
+    //https://github.com/davemorrissey/subsampling-scale-image-view
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_long_screen);
+        SubsamplingScaleImageView imageView  = (SubsamplingScaleImageView) findViewById(R.id.image);
+        //imageView.setImage(ImageSource.asset("long1.jpg"),new ImageViewState(0.5f,new PointF(0,0),0));
+        imageView.setImage(ImageSource.asset("long1.jpg"));
+        imageView.setScaleAndCenter(1f,new PointF(0,0));
+//        imageView.setMinScale(1.5f);
+//        imageView.setMinimumWidth(400);
+//        imageView.setMinimumDpi(10);
+
+        imageView.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_CENTER_CROP);
+    }
+}
